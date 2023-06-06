acf6f80967b0eed81d250efc3772952bbe79f2a3
PotatoPlank <potatoplank@protonmail.com>
2023-06-05 23:27:33 -0400

    Thread repository and entity object

--- src/Spudbot/Model/Thread.php
+++ src/Spudbot/Model/Thread.php
@@ -10,4 +10,24 @@ class Thread extends Model
 {
     private string $discordId;
     private Guild $guild;
+
+    public function setDiscordId(string $discordId): void
+    {
+        $this->discordId = $discordId;
+    }
+
+    public function getDiscordId(): string
+    {
+        return $this->discordId;
+    }
+
+    public function setGuild(Guild $guild): void
+    {
+        $this->guild = $guild;
+    }
+
+    public function getGuild(): Guild
+    {
+        return $this->guild;
+    }
 }
--- src/Spudbot/Repository/SQL/ThreadRepository.php
+++ src/Spudbot/Repository/SQL/ThreadRepository.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace Spudbot\Repository\SQL;
 
+use Carbon\Carbon;
 use Discord\Parts\Part;
+use OutOfBoundsException;
 use Spudbot\Collection;
 use Spudbot\Model;
 use Spudbot\Model\Thread;
@@ -14,7 +16,73 @@ class ThreadRepository extends SQLRepository
 
     public function findById(string $id): Model
     {
-        // TODO: Implement find() method.
+        $queryBuilder = $this->dbal->createQueryBuilder();
+        $response = $queryBuilder->select('*')->from('threads')
+            ->where('id = ?')->setParameters([$id])
+            ->fetchAssociative();
+
+        if(!$response){
+            throw new OutOfBoundsException("Thread with id {$id} does not exist.");
+        }
+
+        $guild = new GuildRepository($this->dbal);
+
+        $thread = new Thread();
+        $thread->setId($response['id']);
+        $thread->setDiscordId($response['discord_id']);
+        $thread->setGuild($guild->findById($response['guild_id']));
+        $thread->setCreatedAt(Carbon::parse($response['created_at']));
+        $thread->setModifiedAt(Carbon::parse($response['modified_at']));
+
+        return $thread;
+    }
+
+    public function findByPart(\Discord\Parts\Thread\Thread|Part $part): Model
+    {
+        $queryBuilder = $this->dbal->createQueryBuilder();
+        $response = $queryBuilder->select('*')->from('threads')
+            ->where('id = ?')->setParameters([$part->id])
+            ->fetchAssociative();
+
+        if(!$response){
+            throw new OutOfBoundsException("Thread with id {$part->id} does not exist.");
+        }
+
+        $guild = new GuildRepository($this->dbal);
+
+        $thread = new Thread();
+        $thread->setId($response['id']);
+        $thread->setDiscordId($response['discord_id']);
+        $thread->setGuild($guild->findById($response['guild_id']));
+        $thread->setCreatedAt(Carbon::parse($response['created_at']));
+        $thread->setModifiedAt(Carbon::parse($response['modified_at']));
+
+        return $thread;
+    }
+
+    public function getAll(): Collection
+    {
+        $collection = new Collection();
+        $guild = new GuildRepository($this->dbal);
+        $queryBuilder = $this->dbal->createQueryBuilder();
+
+        $response = $queryBuilder->select('*')->from('guilds')
+            ->fetchAllAssociative();
+
+        if(!empty($response)){
+            foreach ($response as $row) {
+                $thread = new Thread();
+                $thread->setId($row['id']);
+                $thread->setDiscordId($row['discord_id']);
+                $thread->setGuild($guild->findById($row['guild_id']));
+                $thread->setCreatedAt(Carbon::parse($row['created_at']));
+                $thread->setModifiedAt(Carbon::parse($row['modified_at']));
+
+                $collection->push($thread);
+            }
+        }
+
+        return $collection;
     }
 
     public function save(Thread|Model $model): bool
@@ -25,15 +93,5 @@ class ThreadRepository extends SQLRepository
     public function remove(Thread|Model $model): bool
     {
         // TODO: Implement remove() method.
-    }
-
-    public function findByPart(Part $part): Model
-    {
-        // TODO: Implement findByPart() method.
-    }
-
-    public function getAll(): Collection
-    {
-        // TODO: Implement getAll() method.
     }
 }
