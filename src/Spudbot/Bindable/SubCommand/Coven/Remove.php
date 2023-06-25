70100a88ea9972cdffd45727c925a9d0f3167fe1
PotatoPlank <potatoplank@protonmail.com>
2023-06-24 23:05:24 -0400

    Added the ability to grant Coven access

---
+++ src/Spudbot/Bindable/SubCommand/Coven/Remove.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * This file is a part of the SpudBot Framework.
+ * Copyright (c) 2023. PotatoPlank <potatoplank@protonmail.com>
+ * The file is subject to the GNU GPLv3 license that is bundled with this source code in LICENSE.md.
+ */
+
+declare(strict_types=1);
+
+namespace Spudbot\Bindable\SubCommand\Coven;
+
+
+use Discord\Parts\Interactions\Interaction;
+use Spudbot\Interface\ISubCommand;
+
+class Remove extends ISubCommand
+{
+    protected string $subCommand = 'remove';
+
+    public function execute(?Interaction $interaction): void
+    {
+        if (!$interaction) {
+            throw new \RuntimeException('Remove Coven requires an interaction.');
+        }
+        $builder = $this->spud->getSimpleResponseBuilder();
+        $builder->setTitle('Remove Coven');
+
+        $memberId = $this->options['user']->value;
+        $sourceMemberId = $interaction->member->id;
+        $channelId = '1114365925366440043';
+        $channel = $interaction->guild->channels->get('id', $channelId);
+        $member = $interaction->guild->members->get('id', $memberId);
+        if ($channel && $member) {
+            $channel->setPermissions($member, [], [], 'Moderator removed access.');
+
+            $context = [
+                'memberId' => $memberId,
+                'sourceMemberId' => $sourceMemberId,
+            ];
+            $message = $this->spud->twig->render('user/remove_coven.twig', $context);
+            $builder->setDescription($message);
+        } else {
+            $builder->setDescription('Discord member and/or channel is not yet cached, please try again later.');
+        }
+
+        $interaction->respondWithMessage($builder->getEmbeddedMessage(), true);
+    }
+}
