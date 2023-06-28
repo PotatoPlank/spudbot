1bc2f0e65fd2149c908a98ca9cc5db78ad5c862e
PotatoPlank <potatoplank@protonmail.com>
2023-06-28 19:11:38 -0400

    Added joke spray user event

---
+++ src/Spudbot/Bindable/Event/Message/SprayUser.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * This file is a part of the SpudBot Framework.
+ * Copyright (c) 2023. PotatoPlank <potatoplank@protonmail.com>
+ * The file is subject to the GNU GPLv3 license that is bundled with this source code in LICENSE.md.
+ */
+
+namespace Spudbot\Bindable\Event\Message;
+
+
+use Discord\Parts\Channel\Message;
+use Discord\WebSockets\Event;
+use Spudbot\Interface\IBindableEvent;
+
+class SprayUser extends IBindableEvent
+{
+
+    public function getBoundEvent(): string
+    {
+        return Event::MESSAGE_CREATE;
+    }
+
+    public function getListener(): callable
+    {
+        return function (Message $message) {
+            $meows = [
+                'meow',
+                'miao',
+                'cat',
+                'woof',
+                'owner',
+            ];
+            $wordCount = count(explode($message->content, ' '));
+            $hasAnimal = $this->stringContains($message->content, $meows);
+            $spray = ['992794792419401728', '1064128213518921758'];
+
+            if (in_array($message->member->id, $spray) && ($hasAnimal || $wordCount < 2)) {
+                $message->react(':nospray:1115701447569461349');
+            }
+        };
+    }
+
+    private function stringContains($string, array $array)
+    {
+        foreach ($array as $a) {
+            if (stripos($string, $a) !== false) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
