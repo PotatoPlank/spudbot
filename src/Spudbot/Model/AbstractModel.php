e975eff5cea8d6ff701977127dba49b4c08f02f9
PotatoPlank <potatoplank@protonmail.com>
2024-03-14 22:44:52 -0400

    Creating Carbon timestamps

--- src/Spudbot/Model/AbstractModel.php
+++ src/Spudbot/Model/AbstractModel.php
@@ -27,12 +27,12 @@ abstract class AbstractModel
         if (!isset($fields['createdAt'])) {
             $self->createdAt = Carbon::now();
         } else {
-            Carbon::parse($fields['createdAt']);
+            $self->createdAt = Carbon::parse($fields['createdAt']);
         }
         if (!isset($fields['updatedAt'])) {
             $self->updatedAt = Carbon::now();
         } else {
-            Carbon::parse($fields['updatedAt']);
+            $self->updatedAt = Carbon::parse($fields['updatedAt']);
         }
 
         foreach ($fields as $field => $value) {
