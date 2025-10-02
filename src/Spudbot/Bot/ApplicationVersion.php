cedda4f93c4941b1c0cbea009a764d9cf550c4b6
PotatoPlank <potatoplank@protonmail.com>
2025-10-02 18:38:28 -0400

    Added CI/CD

--- src/Spudbot/Bot/ApplicationVersion.php
+++ src/Spudbot/Bot/ApplicationVersion.php
@@ -26,14 +26,20 @@ class ApplicationVersion
     public static function get(): string
     {
         $version = sprintf('v%d.%d.%d', self::MAJOR, self::MINOR, self::REVISION);
+        $buildFile = dirname(__DIR__, 3) . '/.build-version';
 
-        $buildFile = dirname(__DIR__, 3) . '/build.json';
-        if (!file_exists($buildFile)) {
+        $jsonFile = dirname(__DIR__, 3) . '/build.json';
+        if (!file_exists($jsonFile) && !file_exists($buildFile)) {
             return $version;
         }
-        $contents = file_get_contents($buildFile);
-        $buildDetails = json_decode($contents, false, 512, JSON_THROW_ON_ERROR);
-        self::$buildNumber = $buildDetails->date ?? '';
+
+        if (file_exists($buildFile)){
+            self::$buildNumber = file_get_contents($buildFile);
+        }else{
+            $contents = file_get_contents($buildFile);
+            $buildDetails = json_decode($contents, false, 512, JSON_THROW_ON_ERROR);
+            self::$buildNumber = $buildDetails->date ?? '';
+        }
 
         if (!empty(self::$buildNumber)) {
             $version .= ' - ' . self::$buildNumber;
