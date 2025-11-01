0ca0767b173bd7104b20089522113fd97bd62f3b
PotatoPlank <potatoplank@protonmail.com>
2025-11-01 14:08:11 -0400

    Added startedAt and uptime output to about command

--- src/Spudbot/Commands/About.php
+++ src/Spudbot/Commands/About.php
@@ -1,12 +1,13 @@
 <?php
 /*
  * This file is a part of the SpudBot Framework.
- * Copyright (c) 2024. PotatoPlank <potatoplank@protonmail.com>
+ * Copyright (c) 2024-2025. PotatoPlank <potatoplank@protonmail.com>
  * The file is subject to the GNU GPLv3 license that is bundled with this source code in LICENSE.md.
  */
 
 namespace Spudbot\Commands;
 
+use Carbon\Carbon;
 use Discord\Parts\Interactions\Interaction;
 use Spudbot\Bot\ApplicationVersion;
 
@@ -31,6 +32,8 @@ class About extends AbstractCommandSubscriber
         $message = $this->spud->twig->render('about.twig', [
             'version' => ApplicationVersion::get(),
             'applicationOwnerId' => $this->spud->discord->application->owner->id,
+            'startedAt' => $this->spud->startedAt->format('Y-m-d h:i:s A'),
+            'uptime' => $this->spud->startedAt->diffForHumans(Carbon::now()),
         ]);
 
         $this->spud->interact()
