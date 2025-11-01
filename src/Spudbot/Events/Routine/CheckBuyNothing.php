a746628ca8b439778d94156d6a3b81a46e59448c
PotatoPlank <potatoplank@protonmail.com>
2025-11-01 14:50:44 -0400

    Fixing marketplace thread removal logic and improving associated logging

--- src/Spudbot/Events/Routine/CheckBuyNothing.php
+++ src/Spudbot/Events/Routine/CheckBuyNothing.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * This file is a part of the SpudBot Framework.
- * Copyright (c) 2023-2024. PotatoPlank <potatoplank@protonmail.com>
+ * Copyright (c) 2023-2025. PotatoPlank <potatoplank@protonmail.com>
  * The file is subject to the GNU GPLv3 license that is bundled with this source code in LICENSE.md.
  */
 
@@ -40,29 +40,33 @@ class CheckBuyNothing extends AbstractEventSubscriber
 
     public function update(): void
     {
+        $this->spud->logger->debug('Getting guilds with a marketplace.');
         $guilds = $this->guildService->all(function (Guild $guild) {
             return $guild->hasMarketplace() && $guild->discordId === self::GUILD_TARGET;
         });
         foreach ($guilds as $guild) {
+            $this->spud->logger->debug('Fetching guild with marketplace.', [$guild->getDiscordId()]);
             $part = $this->spud->discord->guilds->get('id', $guild->getDiscordId());
             if (!$part) {
                 continue;
             }
+            $this->spud->logger->debug('Fetching marketplace channel.', [$guild->getDiscordId()]);
             $channel = $this->getMarketplaceChannel($part, $guild);
             if (!$channel) {
                 continue;
             }
-
+            $this->spud->logger->debug('Setting marketplace removable statuses.');
             if (!isset($this->removableStatuses)) {
                 $this->removableStatuses = $channel->available_tags->filter(function ($tag) {
                     return in_array(strtolower($tag->name), ['taken', 'fulfilled'], true);
                 });
             }
-
+            $this->spud->logger->debug('Fetching active marketplace threads.', ['channel' => $channel->id]);
             $active = $channel->threads->active()
                 ->then(fn($threads) => MarketplaceTasks::isInChannel($threads, $channel))
                 ->then(fn($threads) => MarketplaceTasks::getRemovableThreads($threads));
 
+            $this->spud->logger->debug('Fetching archived marketplace threads.', ['channel' => $channel->id]);
             $archived = $channel->threads->archived()
                 ->then(fn($threads) => MarketplaceTasks::isInChannel($threads, $channel))
                 ->then(fn($threads) => MarketplaceTasks::getAgedOrRemovableThreads($threads));
@@ -101,8 +105,11 @@ class CheckBuyNothing extends AbstractEventSubscriber
                         )
                         ->sendTo($output);
                     $channel->threads->delete($thread);
+                    $this->spud->logger->debug(
+                        'Removing marketplace thread.',
+                        ['aged' => $hasAged, 'taken' => $shouldBeRemoved, 'thread' => $thread->name]
+                    );
                 }
-                exit;
             });
         }
     }
